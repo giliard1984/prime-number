749708a50d66df819f56d5f7641b4fca0b826050
Giliard Alves Gomes <giliardalvesgomes@Giliards-MacBook-Pro-2.local>
2024-07-24 17:06:57 +0100

    Adjusted a few things

--- scripts/primeNumberEfficient.js
+++ scripts/primeNumberEfficient.js
@@ -4,25 +4,25 @@
 // Approach: This is an efficient approach as users can type large numbers
 const isPrime = (n) => {
   // 1 is considered neither prime nor composite
-  if (n <= 1) return `Ooops! 1 is neither a prime number nor composite.`;
+  if (n <= 1) return `1 is neither a prime number nor composite.`;
 
   // check if n is equal to 2 or 3.
   // the purpose is to skip processing as we know these numbers are prime
-  if (n == 2 || n == 3) return `Yay! ${n} is a prime number.`;
+  if (n == 2 || n == 3) return `${n} is a prime number.`;
 
   // check whether n is divisible by 2 or 3
   // if a number is divisible by 2 or 3, which means its remaining value is zero, than it is not a prime number
-  if (n % 2 == 0 || n % 3 == 0) return `Ooops! ${n} is not a prime number.`;
+  if (n % 2 == 0 || n % 3 == 0) return `${n} is NOT a prime number.`;
 
   // check from 5 to square root of n
   // we are not checking 4 here, because 4 is divisible by 2, which falls under the previous check
   // tterate i by (i+6)
   for (var i = 5; i <= Math.sqrt(n); i = i + 6) {
     if (n % i == 0 || n % (i + 2) == 0)
-      return `Ooops! ${n} is not a prime number.`;
+      return `${n} is NOT a prime number.`;
   }
       
-  return `Yay! ${n} is a prime number.`;
+  return `${n} is a prime number.`;
 };
 
 const prompts = require('prompts');
--- scripts/primeNumberNaiveApproach.js
+++ scripts/primeNumberNaiveApproach.js
@@ -5,14 +5,14 @@
 // which means a number is divisible by "i", and the remainder is zero.
 const isPrime = (n) => {
   // 1 is considered neither prime nor composite
-  if (n <= 1) return `Ooops! 1 is neither a prime number nor composite.`;
+  if (n <= 1) return `1 is neither a prime number nor composite.`;
 
   // check from 2 to n-1
   for (let i = 2; i < n; i++) {
-    if (n % i == 0) return `Ooops! ${n} is not a prime number.`;
+    if (n % i == 0) return `${n} is NOT a prime number.`;
   }
       
-  return `Yay! ${n} is a prime number.`;
+  return `${n} is a prime number.`;
 };
 
 const prompts = require('prompts');
